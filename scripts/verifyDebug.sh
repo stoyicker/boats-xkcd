8e114e748328ecc04abe50c47ae776968017b6a5
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2015-12-27 13:08:04 +0100

    Work around a Gradle bug. See https://issues.gradle.org/browse/GRADLE-3315 (apparently happening on Ubuntu too).

--- scripts/verifyDebug.sh
+++ scripts/verifyDebug.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+./gradlew --stop &&
 #The assembleDebug task is just to force the method count (no assemble task -> no method count).
 ./gradlew clean lint dependencyUpdates -Drevision=release -DoutputFormatter=plain -DoutputDir=presentation/staticAnalysisReport assembleDebug
