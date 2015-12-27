eb7fc9fefa4f3a6f430a082de9a7adcd65013742
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2015-12-27 14:35:02 +0100

    Remove the clean task so that Travis doesn't get confused by the Gradle daemon bug

--- scripts/verifyDebug.sh
+++ scripts/verifyDebug.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-./gradlew --stop &&
 #The assembleDebug task is just to force the method count (no assemble task -> no method count).
-./gradlew clean lint dependencyUpdates -Drevision=release -DoutputFormatter=plain -DoutputDir=presentation/staticAnalysisReport assembleDebug
+./gradlew lint dependencyUpdates -Drevision=release -DoutputFormatter=plain -DoutputDir=presentation/staticAnalysisReport assembleDebug
