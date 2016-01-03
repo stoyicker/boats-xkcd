6b1f835b5d897f618cfa3d0b0a1c8f91abf93776
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2016-01-03 16:28:37 +0100

    Refactor the navigator in preparation for the upcoming options

--- presentation/src/main/java/com/jorge/boats/navigation/Navigator.java
+++ presentation/src/main/java/com/jorge/boats/navigation/Navigator.java
@@ -1,17 +1,15 @@
 package com.jorge.boats.navigation;
 
 import android.support.annotation.IntDef;
-import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton public class Navigator {
 
-  private static final int NAVIGATION_ITEM_SETTINGS = 0;
+  private static final int NAVIGATION_ITEM_SHARE = 0, NAVIGATION_ITEM_PREVIOUS = 1,
+      NAVIGATION_ITEM_NEXT = 2;
 
-  @IntDef({ NAVIGATION_ITEM_SETTINGS }) public @interface NavigationItem {
-  }
-
-  @SuppressWarnings("MethodNameSameAsClassName") @Inject public void Navigator() {
+  @IntDef({ NAVIGATION_ITEM_SHARE, NAVIGATION_ITEM_PREVIOUS, NAVIGATION_ITEM_NEXT })
+  public @interface NavigationItem {
   }
 
   public void navigateToItem(final @NavigationItem int item) {
