4d5cd70edb9e32dcdfbb651e55bda2b8d1677518
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2016-01-09 22:06:34 +0100

    Fix some lint and script issues

--- scripts/debugVerify.sh
+++ scripts/debugVerify.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-#The assembleDebug task is just to force the method count (no assemble task -> no method count).
-./gradlew lintStandardDebug
+#The assembleStandardDebug task is just to force the method count (no assemble task -> no method count).
+./gradlew lint assembleStandardDebug
