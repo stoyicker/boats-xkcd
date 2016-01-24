58f25c010c6d2e27b84bc7aecfe6480d436fdf46
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2016-01-24 16:29:56 +0100

    Work on UI tests

--- presentation/src/androidTest/java/com/jorge/boats/view/activity/StripeActivityTest.java
+++ presentation/src/androidTest/java/com/jorge/boats/view/activity/StripeActivityTest.java
@@ -16,10 +16,8 @@ import com.jorge.boats.view.stripe.StripeActivity;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.not;
 
 public class StripeActivityTest extends ActivityInstrumentationTestCase2<StripeActivity> {
 
@@ -44,22 +42,14 @@ public class StripeActivityTest extends ActivityInstrumentationTestCase2<StripeA
         getInstrumentation().getTargetContext().getString(R.string.stub_stripe_title));
   }
 
+  @Override protected void tearDown() throws Exception {
+    super.tearDown();
+    mActivity.finish();
+  }
+
   @NonNull private Intent createTargetIntent() {
     return StripeActivity.getCallingIntent(getInstrumentation().getTargetContext(),
         STUB_STRIPE_NUM);
-  }
-
-  public void testStripeLoaded() {
-    final ViewInteraction titleInteraction = onView(withId(R.id.toolbar_title));
-
-    titleInteraction.check(matches(isDisplayed()));
-
-    waitForLoad();
-
-    titleInteraction.check(matches(CustomViewMatchers.withText(equalTo(mStubObject.getTitle()))));
-
-    //We rely on Glide's tests to verify that the correct, image is shown, but we need to ensure that the view is displayed
-    onView(withId(R.id.progress_bar)).check(matches(not(isDisplayed())));
   }
 
   public void testPrevious() {
@@ -86,13 +76,15 @@ public class StripeActivityTest extends ActivityInstrumentationTestCase2<StripeA
 
     waitForLoad();
 
-    new Handler(Looper.getMainLooper()).post(new Runnable() {
+    mActivity.runOnUiThread(new Runnable() {
       @Override public void run() {
         mActivity.findViewById(R.id.fab_index_one).performClick();
       }
     });
 
+    getInstrumentation().waitForIdleSync();
     assertNotNull(receiverActivityMonitor.waitForActivityWithTimeout(SHARE_WAIT_TIME_MILLISECONDS));
+    getInstrumentation().removeMonitor(receiverActivityMonitor);
   }
 
   public void testNext() {
