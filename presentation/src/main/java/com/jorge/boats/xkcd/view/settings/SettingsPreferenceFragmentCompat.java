37f836002dac21535056d4448c871676af61dace
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2016-02-12 23:16:15 +0100

    Modify the 'about xkcd' preference

--- presentation/src/main/java/com/jorge/boats/xkcd/view/settings/SettingsPreferenceFragmentCompat.java
+++ presentation/src/main/java/com/jorge/boats/xkcd/view/settings/SettingsPreferenceFragmentCompat.java
@@ -1,5 +1,8 @@
 package com.jorge.boats.xkcd.view.settings;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -88,6 +91,15 @@ public class SettingsPreferenceFragmentCompat extends PreferenceFragmentCompat {
         com.jorge.boats.xkcd.data.R.string.pref_title_about_xkcd)
         .setMessage(getString(com.jorge.boats.xkcd.data.R.string.pref_message_about_xkcd))
         .setNegativeButton(android.R.string.cancel, null)
+        .setPositiveButton(R.string.read_more, new DialogInterface.OnClickListener() {
+          @Override public void onClick(final @NonNull DialogInterface dialog, final int which) {
+            final String url = getString(R.string.xkcd_base_url);
+            final Intent i = new Intent(Intent.ACTION_VIEW);
+
+            i.setData(Uri.parse(url));
+            startActivity(i);
+          }
+        })
         .create()
         .show();
   }
