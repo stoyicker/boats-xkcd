d98467dc51e798290bea70fb25fc42966f869332
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2016-03-20 13:03:20 +0100

    Fix a bug where dialog preference text linkc colors would not be correct upon theme switch

--- data/src/main/java/com/jorge/boats/xkcd/data/preference/AboutXkcdDialogPreference.java
+++ data/src/main/java/com/jorge/boats/xkcd/data/preference/AboutXkcdDialogPreference.java
@@ -16,53 +16,56 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
+
 import com.jorge.boats.xkcd.data.R;
 
 public class AboutXkcdDialogPreference extends CustomDialogPreference {
 
-  public AboutXkcdDialogPreference(final @NonNull Context context) {
-    super(context);
-  }
+    public AboutXkcdDialogPreference(final @NonNull Context context) {
+        super(context);
+    }
 
-  public AboutXkcdDialogPreference(final @NonNull Context context,
-      final @Nullable AttributeSet attrs) {
-    super(context, attrs);
-  }
+    public AboutXkcdDialogPreference(final @NonNull Context context,
+                                     final @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
 
-  public AboutXkcdDialogPreference(final @NonNull Context context,
-      final @Nullable AttributeSet attrs, final int defStyleAttr) {
-    super(context, attrs, defStyleAttr);
-  }
+    public AboutXkcdDialogPreference(final @NonNull Context context,
+                                     final @Nullable AttributeSet attrs, final int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
 
-  public AboutXkcdDialogPreference(final @NonNull Context context,
-      final @Nullable AttributeSet attrs, final int defStyleAttr, final int defStyleRes) {
-    super(context, attrs, defStyleAttr, defStyleRes);
-  }
+    public AboutXkcdDialogPreference(final @NonNull Context context,
+                                     final @Nullable AttributeSet attrs, final int defStyleAttr, final int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
 
-  @Override
-  public Dialog buildDialog(final @NonNull AlertDialog.Builder builder, final boolean isDark) {
-    final Context context = getContext();
-    @SuppressLint("InflateParams") final View view =
-        ((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(
-            R.layout.preference_about_xkcd, null, false);
-    final TextView body = (TextView) view.findViewById(R.id.body);
+    @Override
+    public Dialog buildDialog(final @NonNull AlertDialog.Builder builder, final boolean isDark) {
+        final Context context = getContext();
+        @SuppressLint("InflateParams") final View view =
+                ((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(
+                        R.layout.preference_about_xkcd, null, false);
+        final TextView body = (TextView) view.findViewById(R.id.body);
 
-    body.setTextColor(isDark ? Color.WHITE : Color.BLACK);
-    body.setMovementMethod(LinkMovementMethod.getInstance());
-    body.setText(Html.fromHtml(context.getString(R.string.pref_message_about_xkcd)));
+        body.setTextColor(isDark ? Color.WHITE : Color.BLACK);
+        body.setMovementMethod(LinkMovementMethod.getInstance());
+        body.setLinkTextColor(super.getLinkColor());
+        body.setText(Html.fromHtml(context.getString(R.string.pref_message_about_xkcd)));
 
-    return builder.setTitle(R.string.pref_title_about_xkcd)
-        .setView(view)
-        .setNegativeButton(android.R.string.cancel, null)
-        .setPositiveButton(R.string.read_more, new DialogInterface.OnClickListener() {
-          @Override public void onClick(final @NonNull DialogInterface dialog, final int which) {
-            final String url = context.getString(R.string.xkcd_base_url);
-            final Intent i = new Intent(Intent.ACTION_VIEW);
+        return builder.setTitle(R.string.pref_title_about_xkcd)
+                .setView(view)
+                .setNegativeButton(android.R.string.cancel, null)
+                .setPositiveButton(R.string.read_more, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(final @NonNull DialogInterface dialog, final int which) {
+                        final String url = context.getString(R.string.xkcd_base_url);
+                        final Intent i = new Intent(Intent.ACTION_VIEW);
 
-            i.setData(Uri.parse(url));
-            context.startActivity(i);
-          }
-        })
-        .create();
-  }
+                        i.setData(Uri.parse(url));
+                        context.startActivity(i);
+                    }
+                })
+                .create();
+    }
 }
--- data/src/main/java/com/jorge/boats/xkcd/data/preference/CustomDialogPreference.java
+++ data/src/main/java/com/jorge/boats/xkcd/data/preference/CustomDialogPreference.java
@@ -3,32 +3,54 @@ package com.jorge.boats.xkcd.data.preference;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.preference.DialogPreference;
 import android.util.AttributeSet;
 
+import com.jorge.boats.xkcd.data.P;
+import com.jorge.boats.xkcd.data.R;
+
+import java.util.Locale;
+
 public abstract class CustomDialogPreference extends DialogPreference {
 
-  public CustomDialogPreference(final @NonNull Context context) {
-    super(context);
-  }
+    public CustomDialogPreference(final @NonNull Context context) {
+        super(context);
+    }
 
-  public CustomDialogPreference(final @NonNull Context context,
-      final @Nullable AttributeSet attrs) {
-    super(context, attrs);
-  }
+    public CustomDialogPreference(final @NonNull Context context,
+                                  final @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
 
-  public CustomDialogPreference(final @NonNull Context context, final @Nullable AttributeSet attrs,
-      final int defStyleAttr) {
-    super(context, attrs, defStyleAttr);
-  }
+    public CustomDialogPreference(final @NonNull Context context, final @Nullable AttributeSet attrs,
+                                  final int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
 
-  public CustomDialogPreference(final @NonNull Context context, final @Nullable AttributeSet attrs,
-      final int defStyleAttr, final int defStyleRes) {
-    super(context, attrs, defStyleAttr, defStyleRes);
-  }
+    public CustomDialogPreference(final @NonNull Context context, final @Nullable AttributeSet attrs,
+                                  final int defStyleAttr, final int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
 
-  public abstract Dialog buildDialog(final @NonNull AlertDialog.Builder builder,
-      final boolean isDark);
+    public abstract Dialog buildDialog(final @NonNull AlertDialog.Builder builder,
+                                       final boolean isDark);
+
+    @ColorInt
+    protected int getLinkColor() {
+        final Context context = getContext();
+        final @ColorInt int ret;
+
+        final TypedArray a = context.getTheme().obtainStyledAttributes(context.getResources().getIdentifier(String.format(Locale.ENGLISH, "Settings%s", P.themeName.get()), "style", context.getPackageName()), new int[]{R.attr.colorAccent});
+
+        ret = a.getColor(0, Color.GREEN);
+
+        a.recycle();
+
+        return ret;
+    }
 }
