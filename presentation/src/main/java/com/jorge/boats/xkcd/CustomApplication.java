bfec20fcce71b72af31c222e3032ea2a1f9a8a0a
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2016-03-20 14:44:59 +0100

    Finish setting up Crashlytics

--- presentation/src/main/java/com/jorge/boats/xkcd/CustomApplication.java
+++ presentation/src/main/java/com/jorge/boats/xkcd/CustomApplication.java
@@ -1,32 +1,45 @@
 package com.jorge.boats.xkcd;
 
 import android.app.Application;
+
+import com.crashlytics.android.Crashlytics;
+import com.jorge.boats.xkcd.BuildConfig;
 import com.jorge.boats.xkcd.data.DataManager;
 import com.jorge.boats.xkcd.di.component.ApplicationComponent;
 import com.jorge.boats.xkcd.di.component.DaggerApplicationComponent;
 import com.jorge.boats.xkcd.di.module.ApplicationModule;
 
+import io.fabric.sdk.android.Fabric;
+
 public class CustomApplication extends Application {
 
-  private ApplicationComponent mApplicationComponent;
+    private ApplicationComponent mApplicationComponent;
 
-  @Override public void onCreate() {
-    super.onCreate();
+    @Override
+    public void onCreate() {
+        super.onCreate();
 
-    this.initializeInjector();
-    this.initializeData();
-  }
+        this.initializeCreepers();
+        this.initializeInjector();
+        this.initializeData();
+    }
 
-  private void initializeInjector() {
-    this.mApplicationComponent =
-        DaggerApplicationComponent.builder().applicationModule(new ApplicationModule(this)).build();
-  }
+    private void initializeCreepers() {
+        if (!BuildConfig.DEBUG) {
+            Fabric.with(this, new Crashlytics());
+        }
+    }
 
-  private void initializeData() {
-    DataManager.initialize(this);
-  }
+    private void initializeInjector() {
+        this.mApplicationComponent =
+                DaggerApplicationComponent.builder().applicationModule(new ApplicationModule(this)).build();
+    }
 
-  public ApplicationComponent getApplicationComponent() {
-    return this.mApplicationComponent;
-  }
+    private void initializeData() {
+        DataManager.initialize(this);
+    }
+
+    public ApplicationComponent getApplicationComponent() {
+        return this.mApplicationComponent;
+    }
 }
