be51897b1bc03a82a1e65746bb722f2df08d01e0
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2016-03-20 14:32:01 +0100

    Log the ComponentNotFound exception.

--- presentation/src/main/java/com/jorge/boats/xkcd/task/UserRetentionGcmTaskService.java
+++ presentation/src/main/java/com/jorge/boats/xkcd/task/UserRetentionGcmTaskService.java
@@ -9,6 +9,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.crashlytics.android.Crashlytics;
 import com.google.android.gms.gcm.GcmNetworkManager;
 import com.google.android.gms.gcm.GcmTaskService;
 import com.google.android.gms.gcm.PeriodicTask;
@@ -32,6 +33,7 @@ public class UserRetentionGcmTaskService extends GcmTaskService {
             return GcmNetworkManager.RESULT_SUCCESS;
         } catch (final @Nullable Exception componentNotRegistered) {
             Log.w(UserRetentionGcmTaskService.class.getName(), componentNotRegistered.getMessage()); // We don't want ApplicationLogger here
+            Crashlytics.logException(componentNotRegistered);
             return GcmNetworkManager.RESULT_RESCHEDULE;
         }
     }
