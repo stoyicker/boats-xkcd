5cd07dfddb3c9a1d0d450013b3b97266e5ffc527
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2016-03-21 20:12:21 +0100

    Fix a bug with random navigation.

--- presentation/src/main/java/com/jorge/boats/xkcd/util/RandomUtil.java
+++ presentation/src/main/java/com/jorge/boats/xkcd/util/RandomUtil.java
@@ -1,22 +1,25 @@
 package com.jorge.boats.xkcd.util;
 
 import android.os.Build;
+
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public abstract class RandomUtil {
 
-  private static final Random GENERATOR = new Random();
+    private static final Random GENERATOR = new Random();
 
-  private RandomUtil() {
-    throw new IllegalAccessError("No instances.");
-  }
-
-  public static long nextLong(final int min, final int max) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-      return ThreadLocalRandom.current().nextLong(min, max + 1);
-    } else {
-      return GENERATOR.nextInt(max + 1 - min) + min;
+    private RandomUtil() {
+        throw new IllegalAccessError("No instances.");
     }
-  }
+
+    public static long nextLong(final int min, final int max) {
+        final int innerMin = Math.max(1, min), innerMax = Math.max(2, max);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            return ThreadLocalRandom.current().nextLong(innerMin, innerMax + 1);
+        } else {
+            return GENERATOR.nextInt(innerMax + 1 - innerMin) + innerMin;
+        }
+    }
 }
