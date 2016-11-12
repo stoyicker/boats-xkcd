2066701c5f9d0a08556f99b3e9e6fd259365fbf4
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@schibsted.com>
2016-11-12 13:43:36 +0100

    API update.

--- presentation/src/test/java/com/jorge/boats/xkcd/PresentationModuleTestCase.java
+++ presentation/src/test/java/com/jorge/boats/xkcd/PresentationModuleTestCase.java
@@ -2,12 +2,15 @@ package com.jorge.boats.xkcd;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
-import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-@RunWith(RobolectricGradleTestRunner.class)
+import static org.junit.Assert.assertTrue;
+
+@RunWith(RobolectricTestRunner.class)
 //TODO Change Robolectric supported SDK to 24 when available
 @Config(constants = BuildConfig.class, application = StubApplication.class, sdk = 23)
 public abstract class PresentationModuleTestCase {
@@ -20,5 +23,10 @@ public abstract class PresentationModuleTestCase {
   @After
   public void tearDown() {
 
+  }
+
+  @Test
+  public void dummy() {
+    assertTrue(true);
   }
 }
